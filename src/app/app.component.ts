49de01992f338369a3b179e8d961cf07cedaed91
Thomas Bée <thomas.bee@twikey.com>
2021-01-20 20:11:02 +0100

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tyed Project';
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
 }
